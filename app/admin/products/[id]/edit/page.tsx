73dd70a61eebe7d33967be946b1c02de75f8f3b3
JuanMichel16 <juan.michel16@outlook.com>
2025-07-25 18:04:18 -0600

    fix: type error about type in edit product fixed

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -4,7 +4,7 @@ import GoBackButton from "@/components/ui/GoBackButton";
 import Heading from "@/components/ui/Heading";
 import { getProductoById } from "@/src/lib/products";
 
-const EditProductPage = async ({params} : {params: {id: string}}) => {
+const EditProductPage = async ({params} : {params: Promise<{id: string}>}) => {
   const productId = await params;
   const product = await getProductoById(productId.id);
 
